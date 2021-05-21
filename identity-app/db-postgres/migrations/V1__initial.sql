c34c4cdb267b8d07d0e57c359c90e1720fd95bda
binhle410 <binh@sunrise.vn>
2021-05-21 13:01:29 +0700

    Clean up un-used folders, rename some Cargo packages

--- identity-app/db-postgres/migrations/V1__initial.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS author_initial
-(
-    id      SERIAL PRIMARY KEY,
-    name    VARCHAR NOT NULL,
-    country VARCHAR NOT NULL
-)
--- identity-app/db-postgres/migrations/V2__PFM1.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO public.author_initial (id, name, country)
-VALUES (2, 'Ngo Dinh Nhu', 'Viet Nam Cong Hoa');
