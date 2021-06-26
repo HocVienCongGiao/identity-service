efcb90954d14d8f0f5c3ba29caab521893e65d99
nhuthm080280 <nhuthm080280@gmail.com>
2021-06-26 12:45:03 +0700

    update test case

--- identity-app/db-postgres/src/test1_gateway/mod.rs
+++ identity-app/db-postgres/src/test1_gateway/mod.rs
@@ -24,6 +24,6 @@ impl domain::boundaries::Test1DbGateway for Test1SimpleRepository {
 
     async fn insert(&self, name: String, country: String) -> bool {
         let result = query::save(&(*self).client, name.clone(), country.clone()).await;
-        return result.is_ok()
+        return result.is_ok();
     }
 }
