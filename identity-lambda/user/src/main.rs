fec07b845d6a19e4d2d0e7d038c8ace0a4f5b2d3
nhuthm080280 <nhuthm080280@gmail.com>
2021-07-05 22:33:55 +0700

    add middle of implement it test

--- identity-lambda/user/src/main.rs
+++ identity-lambda/user/src/main.rs
@@ -20,7 +20,7 @@ async fn create_user(request: Request, context: Context) -> Result<impl IntoResp
         // .unwrap_or_default();
 
     println!("{}", format!(
-        "{}", lambda_user_request.clone().to_string()
+        "payload: {}", lambda_user_request.clone().to_string()
     ));
 
     if request.method() != method::Method::POST {
@@ -32,6 +32,16 @@ async fn create_user(request: Request, context: Context) -> Result<impl IntoResp
     let result = controller::create_user(&lambda_user_request);
 
     return Ok(json!("User is created"))
+    // Example code
+    // async fn func(event: Request, _: Context) -> Result<impl IntoResponse, Error> {
+    //     Ok(match event.query_string_parameters().get("first_name") {
+    //         Some(first_name) => format!("Hello, {}!", first_name).into_response(),
+    //         _ => Response::builder()
+    //             .status(400)
+    //             .body("Empty first name".into())
+    //             .expect("failed to render response"),
+    //     })
+    // }
 }
 
 
@@ -40,25 +50,49 @@ mod tests {
     use super::*;
     use std::path::PathBuf;
     use lambda_http::{RequestExt, Response};
-    use hvcg_example_openapi_entity::models::User;
+    use hvcg_iam_openapi_identity::models::User;
+    use std::collections::HashMap;
 
     #[tokio::test]
     async fn create_user_handles() {
-        let request = Request::default();
-        let expected = json!({
-        "message": "Test 2 is me, how are you?"
-        })
-            .into_response();
-        let response = create_user(request, Context::default())
-            .await
-            .expect("expected Ok(_) value")
-            .into_response();
-        assert_eq!(response.body(), expected.body());
+        let user_request = User {
+            id: Option::from(Uuid::new_v4()),
+            username: "nhuthuynh".to_string(),
+            email: Option::from("nhut_cargo@gmail.com".to_string()),
+            phone: Option::from("0909686868".to_string()),
+        };
+        let serialized_user = serde_json::to_string(&user_request).unwrap();
+        println!("{}", serialized_user);
+
+        let request = Request::new(
+            Body::from(Body::from(serialized_user)
+        ));
+        let test_body = request.body().get(0);
+
+        let lambda_user_request: User = request.payload().unwrap_or_else(|_parse_err| None).unwrap();
+
+        println!("{}", format!(
+            "{}", lambda_user_request.clone().to_string()
+        ));
+
+        // let response = create_user(request, Context::default())
+        //     .await
+        //     .expect("expected Ok(_) value")
+        //     .into_response();
+        // assert_eq!(response.body(), expected.body());
 
 
-        // let user: UserRequest = request.payload()
-        //     .unwrap_or_else(|_parse_err| None)
-        //     .unwrap_or_default();
+        // from the docs
+        // https://docs.aws.amazon.com/lambda/latest/dg/eventsources.html#eventsources-api-gateway-request
+        // let input = include_str!("../tests/data/apigw_v2_proxy_request.json");
+        // let result = from_str(input);
+        // assert!(
+        //     result.is_ok(),
+        //     format!("event was not parsed as expected {:?} given {}", result, input)
+        // );
+        // let req = result.expect("failed to parse request");
+        // assert_eq!(req.method(), "POST");
+        // assert_eq!(req.uri(), "https://id.execute-api.us-east-1.amazonaws.com/my/path?parameter1=value1&parameter1=value2&parameter2=value");
     }
 
 }
