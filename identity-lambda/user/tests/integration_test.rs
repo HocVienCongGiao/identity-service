bc0814ac1c6b2f29f16bfb2dff6170e1cc338ca6
nhuthm080280 <nhuthm080280@gmail.com>
2021-07-20 21:37:30 +0700

    trigger build

--- identity-lambda/user/tests/integration_test.rs
+++ identity-lambda/user/tests/integration_test.rs
@@ -109,7 +109,7 @@ mod tests {
             deserialized_user.phone,
             Option::from("+84 939686970".to_string())
         );
-        println!("Create user successfully.")
+        println!("Create user successfully!")
     }
 
     #[tokio::test]
