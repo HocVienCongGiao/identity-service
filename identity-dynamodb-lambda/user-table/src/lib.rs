4944e8022179ff060fc787fc1d281c6ad7d5aa0c
nhuthm080280 <nhuthm080280@gmail.com>
2021-07-25 21:19:40 +0700

    dynamodb query user

--- identity-dynamodb-lambda/user-table/src/lib.rs
+++ identity-dynamodb-lambda/user-table/src/lib.rs
@@ -1,14 +1,52 @@
 use lambda_runtime::{Context, Error};
 use serde_json::{json, Value};
+// use hvcg_iam_openapi_identity::models::User;
+use rusoto_core::credential::EnvironmentProvider;
+use rusoto_core::{HttpClient, Region};
+use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient, ListTablesInput, PutItemInput, GetItemInput};
+use std::collections::HashMap;
 
 pub async fn func(event: Value, _: Context) -> Result<Value, Error> {
     // let first_name = event["firstName"].as_str().unwrap_or("world");
     println!("welcome to dynamodb processor!!!!");
     println!("Event payload is {:?}", event);
 
-    println!("{}", (json!({ "message": format!("Hello, {:?}!", event) })));
+    let hash_key = event["Records"].get(0)
+        .and_then(|value| value.get("Keys"))
+        .and_then(|value| value.get("HashKey"))
+        .and_then(|value| value.get("S"))
+        .unwrap().to_string();
+    println!("hash_key: {}", hash_key);
 
     // Get item by hash key
+    let client = DynamoDbClient::new_with(
+        HttpClient::new().unwrap(),
+        EnvironmentProvider::default(),
+        Region::ApSoutheast1,
+    );
+
+    // Filter condition
+
+    let mut query_condition: HashMap<String, AttributeValue> = HashMap::new();
+    query_condition.insert(
+        String::from("HashKey"),
+        AttributeValue {
+            s: Option::from(hash_key),
+            ..Default::default()
+        },
+    );
+    let user_table_name = "dev-sg_UserTable".to_string();
+    let user = client.get_item(
+        GetItemInput {
+            attributes_to_get: None,
+            consistent_read: None,
+            expression_attribute_names: None,
+            key: query_condition,
+            projection_expression: Option::from("id, username, email, phone".to_string()),
+            return_consumed_capacity: None,
+            table_name:user_table_name
+        }
+    ).sync();
     // upsert to cognito
     Ok(json!({ "message": format!("Hello, {:?}!", event) }))
 }
@@ -19,69 +57,81 @@ mod tests {
     use std::collections::HashMap;
 
     use serde_json::{json, Map, Value};
-
+    use rusoto_core::credential::EnvironmentProvider;
+    use rusoto_core::{HttpClient, Region};
+    use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient, ListTablesInput, PutItemInput, GetItemInput};
     use crate::func;
+    use std::env;
 
     #[tokio::test]
     async fn create_user_success() {
-        let mut records: Map<String, Value> = Default::default();
-        records.insert("Records".parse().unwrap(), Value::Array(vec![]));
-
-        let value = Value::Object(
-            records
+        // Get item by hash key
+        let client = DynamoDbClient::new_with(
+            HttpClient::new().unwrap(),
+            EnvironmentProvider::default(),
+            Region::ApSoutheast1,
         );
-        func(value, Default::default()).await;
-        // let v = json!({ "a": ["an", "array"], "b": { "an": "object" } });
-        // let records = v["a"].as_array().unwrap();
-        // 
-        // // The length of `["an", "array"]` is 2 elements.
-        // assert_eq!(v["a"].as_array().unwrap().len(), 2);
-        // 
-        // // The object `{"an": "object"}` is not an array.
-        // assert_eq!(v["b"].as_array(), None);
 
-        // Object(
-        //     {
-        //         "Records": Array(
-        //             [
-        //                 Object(
-        //         {
-        //             "awsRegion": String(
-        //             "ap-southeast-1"),
-        //             "dynamodb": Object(
-        //             {
-        //                 "ApproximateCreationDateTime": Number(
-        //                 1627141532.0),
-        //                 "Keys": Object(
-        //                 {
-        //                     "HashKey": Object(
-        //                     {
-        //                         "S": String(
-        //                         "11905088586532604268")
-        //                     })
-        //                 }),
-        //                 "SequenceNumber": String(
-        //                 "14562700000000010320701664"),
-        //                 "SizeBytes": Number(
-        //                 27),
-        //                 "StreamViewType": String(
-        //                 "KEYS_ONLY")
-        //             }),
-        //             "eventID": String(
-        //             "6ad7a20462f36feb8632ccbd9e517a00"),
-        //             "eventName": String(
-        //             "INSERT"),
-        //             "eventSource": String(
-        //             "aws:dynamodb"),
-        //             "eventSourceARN": String(
-        //             "arn:aws:dynamodb:ap-southeast-1:891616054205:table/dev-sg_UserTable/stream/2021-07-21T10:43:28.424"),
-        //             "eventVersion": String(
-        //             "1.1")
-        //         }
-        //         )
-        //         ]
-        //         )
-        //     }
-        // )
+        // Filter condition
+
+        let mut query_condition: HashMap<String, AttributeValue> = HashMap::new();
+        query_condition.insert(
+            String::from("HashKey"),
+            AttributeValue {
+                s: Option::from("6790795613568784684".to_string()),
+                ..Default::default()
+            },
+        );
+
+        let user_table_name = "dev-sg_UserTable".to_string();
+        let user = client.get_item(
+            GetItemInput {
+                attributes_to_get: None,
+                consistent_read: None,
+                expression_attribute_names: None,
+                key: query_condition,
+                projection_expression: Option::from("hash_key, HashKey".to_string()),
+                return_consumed_capacity: None,
+                table_name: user_table_name
+            }
+        ).sync();
+
+        println!("user_dynamodb {:?}", user.unwrap());
+
+        // let mut records: Map<String, Value> = Default::default();
+        // let mut aws_object: Map<String, Value> = Default::default();
+        // let mut hash_key_object: Map<String, Value> = Default::default();
+        // let mut hash_key_object_details: Map<String, Value> = Default::default();
+        // hash_key_object_details.insert("S".to_string(), Value::String("11905088586532604268".to_string()));
+        // hash_key_object.insert("HashKey".to_string(),
+        //                        Value::Object(hash_key_object_details));
+        // // "Keys": Object({"HashKey": Object({"S": String("11905088586532604268")})})
+        // aws_object.insert("Keys".to_string(), Value::Object(hash_key_object));
+        // records.insert(
+        //     "Records".parse().unwrap(),
+        //     Value::Array(
+        //         vec![Value::Object(aws_object)]
+        //     )
+        // );
+
+        // Testing purpose
+        // let hash_key = records["Records"].get(0)
+        //     .and_then(|value| value.get("Keys"))
+        //     .and_then(|value| value.get("HashKey"))
+        //     .and_then(|value| value.get("S"))
+        //     .unwrap().to_string();
+        // let hostname: Option<&str> = records.get("Records")
+        //     .and_then(|value| value.get(0))
+        //     .and_then(|value| value.get("HashKey"))
+        //     .and_then(|value| value.as_str());
+        // nhut_hash_key: {"Records": Array([Object({"Keys":
+        // Object({"HashKey": Object({"S": String("11905088586532604268")})})})])}
+        // println!("nhut_hash_key: {:?}", hash_key);
+
+        // let value = Value::Object(
+        //     records
+        // );
+        // let result = func(value, Default::default()).await;
+
     }
 }
