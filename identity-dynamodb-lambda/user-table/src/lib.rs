3c8c144c36a75c44f18efe3f1903761ba0653177
nhuthm080280 <nhuthm080280@gmail.com>
2021-07-28 21:57:50 +0700

    able to insert user to cognito

--- identity-dynamodb-lambda/user-table/src/lib.rs
+++ identity-dynamodb-lambda/user-table/src/lib.rs
@@ -36,7 +36,7 @@ pub async fn func(event: Value, _: Context) -> Result<Value, Error> {
     query_condition.insert(
         String::from("HashKey"),
         AttributeValue {
-            s: Option::from(hash_key),
+            s: Option::from(hash_key.replace("\"", "")),
             ..Default::default()
         },
     );
@@ -121,13 +121,17 @@ mod tests {
 
     use crate::func;
     use rusoto_core::credential::EnvironmentProvider;
-    use rusoto_core::{HttpClient, Region};
+    use rusoto_core::{HttpClient, Region, Client};
     use rusoto_dynamodb::{
         AttributeValue, DynamoDb, DynamoDbClient, GetItemInput, ListTablesInput, PutItemInput,
     };
     use serde_json::{json, Map, Value};
     use std::env;
     use std::path::PathBuf;
+    use rusoto_cognito_idp::{AdminCreateUserRequest, AttributeType, CognitoIdentityProviderClient, CognitoIdentityProvider};
+    use std::sync::Once;
+
+    static INIT: Once = Once::new();
 
     fn initialise() {
         INIT.call_once(|| {
@@ -139,80 +143,132 @@ mod tests {
 
     #[tokio::test]
     async fn create_user_success() {
+        initialise();
+        env::set_var("AWS_ACCESS_KEY_ID", std::env::var("AWS_ACCESS_KEY_ID").unwrap());
+        env::set_var(
+            "AWS_SECRET_ACCESS_KEY",
+            std::env::var("AWS_SECRET_ACCESS_KEY").unwrap(),
+        );
+
+        let mut records: Map<String, Value> = Default::default();
+        let mut aws_object: Map<String, Value> = Default::default();
+        let mut hash_key_object: Map<String, Value> = Default::default();
+        let mut hash_key_object_details: Map<String, Value> = Default::default();
+        hash_key_object_details.insert("S".to_string(), Value::String("6790795613568784684".to_string()));
+        hash_key_object.insert("HashKey".to_string(),
+                               Value::Object(hash_key_object_details));
+        // "Keys": Object({"HashKey": Object({"S": String("11905088586532604268")})})
+        aws_object.insert("Keys".to_string(), Value::Object(hash_key_object));
+        records.insert(
+            "Records".parse().unwrap(),
+            Value::Array(
+                vec![Value::Object(aws_object)]
+            )
+        );
+
+        let event = Value::Object(
+            records
+        );
+
+        let mut hash_key = event["Records"]
+            .get(0)
+            .and_then(|value| value.get("Keys"))
+            .and_then(|value| value.get("HashKey"))
+            .and_then(|value| value.get("S"))
+            .unwrap()
+            .to_string();
+        println!("hash_key: {}", hash_key.replace("\"", ""));
+
         // Get item by hash key
-        // let client = DynamoDbClient::new_with(
-        //     HttpClient::new().unwrap(),
-        //     EnvironmentProvider::default(),
-        //     Region::ApSoutheast1,
-        // );
-        //
-        // // Filter condition
-        //
-        // let mut query_condition = HashMap::new();
-        // query_condition.insert(
-        //     String::from("HashKey"),
-        //     AttributeValue {
-        //         s: Option::from("6790795613568784684".to_string()),
-        //         ..Default::default()
-        //     },
-        // );
-        //
-        // let user_table_name = "dev-sg_UserTable".to_string();
-        // let user = client
-        //     .get_item(GetItemInput {
-        //         attributes_to_get: None,
-        //         consistent_read: None,
-        //         expression_attribute_names: None,
-        //         key: query_condition,
-        //         projection_expression: None,
-        //         return_consumed_capacity: None,
-        //         table_name: user_table_name,
-        //     })
-        //     .sync();
-        //
-        // let test = user
-        //     .unwrap()
-        //     .item
-        //     .unwrap()
-        //     .get("username")
-        //     .and_then(|value| value.s.clone())
-        //     .unwrap();
-        //
-        // println!("user_dynamodb {:?}", test);
+        let client = DynamoDbClient::new_with(
+            HttpClient::new().unwrap(),
+            EnvironmentProvider::default(),
+            Region::ApSoutheast1,
+        );
 
-        // let mut records: Map<String, Value> = Default::default();
-        // let mut aws_object: Map<String, Value> = Default::default();
-        // let mut hash_key_object: Map<String, Value> = Default::default();
-        // let mut hash_key_object_details: Map<String, Value> = Default::default();
-        // hash_key_object_details.insert("S".to_string(), Value::String("11905088586532604268".to_string()));
-        // hash_key_object.insert("HashKey".to_string(),
-        //                        Value::Object(hash_key_object_details));
-        // // "Keys": Object({"HashKey": Object({"S": String("11905088586532604268")})})
-        // aws_object.insert("Keys".to_string(), Value::Object(hash_key_object));
-        // records.insert(
-        //     "Records".parse().unwrap(),
-        //     Value::Array(
-        //         vec![Value::Object(aws_object)]
-        //     )
-        // );
+        // Filter condition
+        let mut query_condition: HashMap<String, AttributeValue> = HashMap::new();
+        query_condition.insert(
+            String::from("HashKey"),
+            AttributeValue {
+                s: Option::from(hash_key.replace("\"", "")),
+                ..Default::default()
+            },
+        );
+        let user_table_name = "dev-sg_UserTable".to_string();
+        let user = client
+            .get_item(GetItemInput {
+                attributes_to_get: None,
+                consistent_read: None,
+                expression_attribute_names: None,
+                key: query_condition,
+                projection_expression: None,
+                return_consumed_capacity: None,
+                table_name: user_table_name,
+            })
+            .sync();
+        println!("user : {:?}", user);
+        let username = user
+            .as_ref()
+            .unwrap()
+            .item
+            .as_ref()
+            .unwrap()
+            .get("username")
+            .and_then(|value| value.s.clone());
+        println!("{}", username.as_ref().unwrap());
+        let email = user
+            .as_ref()
+            .unwrap()
+            .item
+            .as_ref()
+            .unwrap()
+            .get("email")
+            .and_then(|value| value.s.clone());
+        println!("{}", email.as_ref().unwrap());
+        let phone = user
+            .unwrap()
+            .item
+            .unwrap()
+            .get("phone")
+            .and_then(|value| value.s.clone());
+        println!("{}", phone.as_ref().unwrap());
+        // Insert user to cognito
+        let aws_client = Client::shared();
+        let user_pool_id = "ap-southeast-1_9QWSYGzXk".to_string();
+        let rusoto_cognito_idp_client =
+            CognitoIdentityProviderClient::new_with_client(aws_client, Region::ApSoutheast1);
 
-        // Testing purpose
-        // let hash_key = records["Records"].get(0)
-        //     .and_then(|value| value.get("Keys"))
-        //     .and_then(|value| value.get("HashKey"))
-        //     .and_then(|value| value.get("S"))
-        //     .unwrap().to_string();
-        // let hostname: Option<&str> = records.get("Records")
-        //     .and_then(|value| value.get(0))
-        //     .and_then(|value| value.get("HashKey"))
-        //     .and_then(|value| value.as_str());
-        // nhut_hash_key: {"Records": Array([Object({"Keys":
-        // Object({"HashKey": Object({"S": String("11905088586532604268")})})})])}
-        // println!("nhut_hash_key: {:?}", hash_key);
+        let mut user_attributes: Vec<AttributeType> = vec![
+            AttributeType {
+                name: "email".to_string(),
+                value: email,
+            },
+            AttributeType {
+                name: "phone_number".to_string(),
+                value: Option::from("+84369140916".to_string()),
+            }
+        ];
 
-        // let value = Value::Object(
-        //     records
-        // );
-        // let result = func(value, Default::default()).await;
+        let admin_create_user_request = AdminCreateUserRequest {
+            desired_delivery_mediums: None,
+            force_alias_creation: None,
+            message_action: None,
+            temporary_password: None,
+            user_attributes: Option::from(user_attributes),
+            user_pool_id,
+            username: username.unwrap(),
+            validation_data: None,
+        };
+
+        let result_cognito = rusoto_cognito_idp_client
+            .admin_create_user(admin_create_user_request)
+            .sync();
+        if result_cognito.is_err() {
+            println!("Error: {:?}", result_cognito.as_ref().err());
+        }
+
+        println!("Result: {:?}", result_cognito.unwrap())
+
     }
 }
