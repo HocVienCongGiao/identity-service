e065c0c872f6e1ba685b1de04b82698ac22e7f56
nhuthm080280 <nhuthm080280@gmail.com>
2021-08-07 19:14:04 +0700

    update package name

--- identity-lambda/user_deactivation/src/main.rs
+++ identity-lambda/user_deactivation/src/main.rs
@@ -1,7 +1,7 @@
 mod lib;
 type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
 use lambda_http::{handler, lambda_runtime};
-use user_deactivate::deactivate_user;
+use user_deactivation::deactivate_user;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
