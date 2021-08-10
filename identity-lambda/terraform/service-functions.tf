efbcf2a46bfa453aceb82c3c8de79197c5520cb5
nhuthm080280 <nhuthm080280@gmail.com>
2021-08-10 21:40:48 +0700

    revert user function

--- identity-lambda/terraform/service-functions.tf
+++ identity-lambda/terraform/service-functions.tf
@@ -38,6 +38,26 @@ module "users_id" {
   db_name              = var.db_name
 }
 
+module "user" {
+  source = "git::ssh://git@github.com/HocVienCongGiao/terraform-infra.git//skeleton/services/service-function"
+  service_name = var.service_name
+  query_api_parent_id = module.identity-service.query_api_gateway_resource_id
+  mutation_api_parent_id = module.identity-service.mutation_api_gateway_resource_id
+
+  function_name = "user"
+  is_query_api  = false
+
+  depends_on = [
+    module.identity-service
+  ]
+
+  environment = var.environment
+  db_host              = var.db_host
+  db_user              = var.db_user
+  db_password          = var.db_password
+  db_name              = var.db_name
+}
+
 module "users_deactivation" {
   source = "git::ssh://git@github.com/HocVienCongGiao/terraform-infra.git//skeleton/services/service-function"
   service_name = var.service_name
