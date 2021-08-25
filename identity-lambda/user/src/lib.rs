ea3c52e363dc72f0b06c41568b18ed997c80987b
nhuthm080280 <nhuthm080280@gmail.com>
2021-08-25 21:28:39 +0700

    fix get user by id

--- identity-lambda/user/src/lib.rs
+++ identity-lambda/user/src/lib.rs
@@ -39,7 +39,7 @@ pub async fn func(request: Request, context: Context) -> Result<impl IntoRespons
             let request_user_id = get_id_from_request(&request);
             if request_user_id.is_some() {
                 let id = request_user_id.unwrap();
-                println!("Get user by id: {}", id.unwrap().clone());
+                println!("Get user by id: {}", id);
                 user_collection = None;
                 user_response = controller::get_user_by_id(id).await;
                 status_code = 200;
