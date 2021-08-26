b6b432583cca6a5f5ee5f59e13977da5115b31bb
nhuthm080280 <nhuthm080280@gmail.com>
2021-08-26 20:25:58 +0700

    fix user group response for get users api

--- identity-app/db-postgres/src/user_gateway/mod.rs
+++ identity-app/db-postgres/src/user_gateway/mod.rs
@@ -245,13 +245,40 @@ impl domain::boundaries::UserDbGateway for UserRepository {
         let pagination = combine_into_pagination_string(offset, count);
         let result = query::get_users(&(*self).client, filter.clone(), pagination).await;
 
-        let collection: Vec<UserDbResponse>;
-        collection = result
-            .unwrap()
-            .into_iter()
-            .map(|row| convert_to_user_db_response(row, vec![]))
-            .collect();
-
+        let mut collection: Vec<UserDbResponse> = vec![];
+        // collection = result
+        //     .unwrap()
+        //     .into_iter()
+        //     .map(|row| convert_to_user_db_response(row, vec![]))
+        //     .collect();
+        for row in result.unwrap() {
+            let mut user_groups = get_user_group_by_user_id(&(*self).client, row.get("id"))
+                .await
+                .unwrap();
+            if user_groups.is_empty() {
+                println!("User group is empty");
+            }
+            let mut group_ids = vec![];
+            for row in user_groups {
+                let group_id = row.get("group_id");
+                group_ids.push(group_id);
+            }
+            println!("group_ids: {:?}", group_ids);
+            let mut groups = get_groups_by_group_id(&(*self).client, group_ids)
+                .await
+                .unwrap();
+            println!("groups_result: {:?}", groups);
+            if groups.is_empty() {
+                println!("Groups is empty");
+            }
+            let mut group_names: Vec<String> = vec![];
+            for group in groups {
+                let group_name: String = group.get("group_name");
+                group_names.push(group_name)
+            }
+            let user_db_response = convert_to_user_db_response(row, group_names);
+            collection.push(user_db_response);
+        }
         let has_more: Option<bool>;
         if let Some(count_param) = count {
             let pagination = combine_into_pagination_string(offset, None);
@@ -387,7 +414,7 @@ fn combine_into_pagination_string(offset: Option<u16>, count: Option<u16>) -> St
     format!("LIMIT {} OFFSET {}", count, offset)
 }
 
-fn convert_to_user_db_response(row: Row, group_name: Vec<String> ) -> UserDbResponse {
+fn convert_to_user_db_response(row: Row, group_name: Vec<String>) -> UserDbResponse {
     let id: Uuid = row.get("id");
     let username: String = row.get("username");
     let phone: String = row.get("phone");
