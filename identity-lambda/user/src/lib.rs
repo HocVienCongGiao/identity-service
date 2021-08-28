880701fd1b31560acd42ce4eb66d1dfd6e52f9af
nhuthm080280 <nhuthm080280@gmail.com>
2021-08-28 14:42:40 +0700

    handle options request method

--- identity-lambda/user/src/lib.rs
+++ identity-lambda/user/src/lib.rs
@@ -8,6 +8,7 @@ use lambda_http::http::header::{
 use lambda_http::http::{method, HeaderValue, Method, StatusCode};
 use lambda_http::{Body, Context, IntoResponse, Request, RequestExt, Response};
 use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 use uuid::Uuid;
 
 type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
@@ -24,7 +25,53 @@ struct TokenPayload {
 
 pub async fn func(request: Request, context: Context) -> Result<impl IntoResponse, Error> {
     println!("Request {:?}", request);
+    println!("path_parameters {:?}", request.path_parameters());
+    println!(
+        "query_string_parameters {:?}",
+        request.query_string_parameters()
+    );
     println!("Request Method {:?}", request.method());
+    if request.method() == method::Method::OPTIONS {
+        return Ok(Response::builder()
+            .header(CONTENT_TYPE, "application/json")
+            .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+            .header(ACCESS_CONTROL_ALLOW_HEADERS, "*")
+            .header(ACCESS_CONTROL_ALLOW_METHODS, "*")
+            .status(200)
+            .body(Body::Empty)
+            .expect("unable to build http::Response"));
+    }
+    let default_header_value = HeaderValue::from_str("Bearer eyJraWQiOiJaTGpneG41SStaZEpldnJRb0lpMTZEWEZoRHI4eG9UbVZ2b2ZuVm5vb3RFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJmZDlhN2FmOC1mYTc2LTRiODYtYWYzZC1kOTYzNGVmNTIzNzQiLCJhdWQiOiIxcmF2NDExbmNjbnA3M2h0b3BiaG1sOHM2MSIsImNvZ25pdG86Z3JvdXBzIjpbIk9wZXJhdG9yR3JvdXAiXSwiZXZlbnRfaWQiOiI5NjQ1ZDYyMi0zZjRiLTQyYjctOWI0ZC03MWQzNWRhOTI1NmQiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTYyMzkzNDkyNiwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmFwLXNvdXRoZWFzdC0xLmFtYXpvbmF3cy5jb21cL2FwLXNvdXRoZWFzdC0xXzlRV1NZR3pYayIsInBob25lX251bWJlcl92ZXJpZmllZCI6dHJ1ZSwiY29nbml0bzp1c2VybmFtZSI6ImRldi1vcGVyYXRvciIsInBob25lX251bWJlciI6Iis4NDM2OTE0MDkxNiIsImV4cCI6MTYyMzk0ODAwOCwiaWF0IjoxNjIzOTQ0NDA4fQ.ml3N8J7uw4rbQOneEdnmQW6OwsAY6ycmp5PIrKGZKF3yWQn0oQECIhF2Q_jjWOjWPikpUQEy5IKgghiJLukgKo7q-T4tUauPG3GJxoSGQkfVcglkNu8nZTu7ioxXzlQAWsXLakgkH40mGzI6kl2hkEhRQh_lWGrT7TqDP2yVTsDMKEGJBdtcb-kFCnYHfn9FMoCyVGo4K3tSrkeGno7bzwO_XpFtZRhv9Qs4OtfESXARYCP3St69hyf4JuAop6-Zb38FPWcp6rnpRG3BF64YPGqo0J0MAyWVz_Du7Pk3-H5uZqqrr6iHKoPwoabPPlZxJ3JGdifVt_I54SwTbelbzw").unwrap();
+    let auth_header_value = request
+        .headers()
+        .get("authorization")
+        .unwrap_or(&default_header_value);
+    let auth_header_str = auth_header_value.to_str().unwrap();
+    let username: String;
+    let groups: Vec<String>;
+    if auth_header_str != "anonymous12" {
+        let jwt_token = &auth_header_str.to_string()[7..];
+        let token_data: TokenData<TokenPayload> =
+            jsonwebtoken::dangerous_insecure_decode(jwt_token).unwrap();
+        let token_payload = token_data.claims;
+        username = token_payload.username;
+        groups = token_payload.groups;
+        println!("Groups include {:?}", groups);
+    } else {
+        username = String::from("anonymous");
+    }
+    println!("token username {}", username);
+    // creating an application/json response
+    // Ok(json!({
+    // "message": "Test 2 is me, how are you?"
+    // }))
+    println!("auth_header is {}", auth_header_str);
+    println!("req.headers() is {:?}", request.headers());
+    let value = json!(
+        {
+            "message": "Test 2 20210616 is me, how are you?"
+        }
+    );
     let status_code: u16;
     let user_response: Option<controller::openapi::identity_user::User>;
     let invoked_function_arn = context.invoked_function_arn;
@@ -33,6 +80,7 @@ pub async fn func(request: Request, context: Context) -> Result<impl IntoRespons
     let function_name = get_post_function_name(request.uri().to_string());
     let mut is_get_users = false;
     let user_collection: Option<UserCollection>;
+
     match *request.method() {
         method::Method::GET => {
             println!("Handle get method.");
